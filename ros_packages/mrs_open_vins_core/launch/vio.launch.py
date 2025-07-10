f2817e37546a93660bf4270dd9029549e7b688ad
Jakub Lenner <lennejak@fel.cvut.cz>
2025-07-10 15:45:57 +0200

    added custom config and remappings and everything

--- ros_packages/mrs_open_vins_core/launch/vio.launch.py
+++ ros_packages/mrs_open_vins_core/launch/vio.launch.py
@@ -1,54 +1,102 @@
+import yaml
+import os
+
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, OpaqueFunction, LogInfo
 from launch.substitutions import LaunchConfiguration, EnvironmentVariable, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 
+def get_processed_launch_objects(context):
+    _custom_config_file = LaunchConfiguration('custom_config').perform(context)
+    remappings = []
+    executable_name = "run_subscribe_msckf"
+    prefix = executable_name
+    # pull remapping of the topics out of the yaml file
+    if _custom_config_file != '':
+        with open(_custom_config_file, 'r') as f:
+            yaml_data = yaml.load(f, Loader=yaml.FullLoader)
+
+            if prefix in yaml_data and 'remappings' in yaml_data[prefix]['ros__parameters']:
+                remappings_subyaml = yaml_data[prefix]['ros__parameters']['remappings']
+                for orig_name in remappings_subyaml:
+                    new_name = remappings_subyaml[orig_name]
+                    #print(f"remapping topic {orig_name} to {new_name}")
+                    remappings.append((orig_name, new_name))
+                    
+    objects = [
+        LogInfo(msg=f"custom config file: {_custom_config_file}"),
+        LogInfo(msg=f"remappings:"),
+    ]
+        
+    for remapping in remappings:
+        objects.append(LogInfo(msg=f"\t{remapping[0]} -> {remapping[1]}"))
+    
+    
+    objects.append(IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('bluefox2'),
+                'launch',
+                'single.launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'custom_config': LaunchConfiguration('custom_config')
+        }.items()
+    ))
+    
+    objects.append(IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('mrs_serial'),
+                'launch',
+                'vio_imu.launch.py'
+            ])
+        ])
+    ))
+    
+    objects.append(DeclareLaunchArgument(name='uav_name',          default_value=os.environ["UAV_NAME"]))
+    objects.append(DeclareLaunchArgument(name='config',            default_value='realworld_bluefox_front',  description=''))
+    objects.append(DeclareLaunchArgument(name='verbosity',         default_value='DEBUG',       description='ALL, DEBUG, INFO, WARNING, ERROR, SILENT'))
+    #objects.append(DeclareLaunchArgument(name='use_stereo',        default_value='false',       description=''))
+    #objects.append(DeclareLaunchArgument(name='max_cameras',       default_value='1',          description=''))
+    objects.append(Node(package = 'ov_msckf',
+        executable = executable_name,
+        namespace = LaunchConfiguration('uav_name'),
+        parameters =[{'verbosity': LaunchConfiguration('verbosity')},
+                    {'config_path': PathJoinSubstitution([
+                                        FindPackageShare('mrs_open_vins_core'),
+                                        'config',
+                                        LaunchConfiguration('config'),
+                                        'estimator_config.yaml'
+                                    ])
+                    },
+                    # {'topic_imu', '/imu_raw'},
+                    # {'topic_camera', '/uav1/image_raw'}
+                ],
+        remappings=remappings
+        # remappings=[('/cam0/image_raw', '/uav1/image_raw'),
+        #             ('/imu0', '/imu_raw')]
+        #prefix="xterm -e gdb -ex=r --args",
+        #prefix="gdb -ex=r --args",
+    ))
+
+    return objects
+
+
 def generate_launch_description():
     return LaunchDescription([
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                PathJoinSubstitution([
-                    FindPackageShare('bluefox2'),
-                    'launch',
-                    'single.launch.py'
-                ])
+        DeclareLaunchArgument(
+            'custom_config',
+            default_value=PathJoinSubstitution([
+                FindPackageShare('mrs_open_vins_core'),
+                'config',
+                'realworld_bluefox_front',
+                'custom_config_exmaple.yaml'
             ]),
+            description='config from the user'
         ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                PathJoinSubstitution([
-                    FindPackageShare('mrs_serial'),
-                    'launch',
-                    'vio_imu.launch.py'
-                ])
-            ]),
-        ),
-        DeclareLaunchArgument(name='namespace',         default_value='',           description='namespace'),
-        DeclareLaunchArgument(name='config',            default_value='realworld_bluefox_front',  description=''),
-        DeclareLaunchArgument(name='verbosity',         default_value='DEBUG',       description='ALL, DEBUG, INFO, WARNING, ERROR, SILENT'),
-        DeclareLaunchArgument(name='use_stereo',        default_value='false',       description=''),
-        DeclareLaunchArgument(name='max_cameras',       default_value='1',          description=''),
-        Node(package = 'ov_msckf',
-            executable = 'run_subscribe_msckf',
-            namespace = LaunchConfiguration('namespace'),
-            parameters =[{'verbosity': LaunchConfiguration('verbosity')},
-                        #{'use_stereo': LaunchConfiguration('use_stereo')},
-                        #{'max_cameras': LaunchConfiguration('max_cameras')},
-                        {'config_path': PathJoinSubstitution([
-                                            FindPackageShare('mrs_open_vins_core'),
-                                            'config',
-                                            LaunchConfiguration('config'),
-                                            'estimator_config.yaml'
-                                        ])
-                        },
-                        {'topic_imu', '/imu_raw'},
-                        {'topic_camera', '/uav1/image_raw'}
-                    ],
-            remappings=[('/cam0/image_raw', '/uav1/image_raw'),
-                        ('/imu0', '/imu_raw')]
-            #prefix="xterm -e gdb -ex=r --args",
-            #prefix="gdb -ex=r --args",
-        )
+        OpaqueFunction(function=get_processed_launch_objects)
     ])
